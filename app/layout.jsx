d461ea2a037546d5ac9ec364ae92dc88183bb00e
Marius Korbmacher <marius@korbmacher.me>
2026-03-25 17:54:02 +0100

    feat: add favicons

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,6 +15,18 @@ export const viewport = {
 
 export const metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL),
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+  },
+  manifest: "/site.webmanifest",
+  appleWebApp: {
+    title: "FiveOh",
+  },
   title,
   description,
   openGraph: {
